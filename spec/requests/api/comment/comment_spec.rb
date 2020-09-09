b940241446f4393767f725d6989f4184d6c74d2d
Sima <sima.boreiri@gmail.com>
2020-09-09 13:21:15 +0200

    creates the test file and write the first test for comment, adds devise token auth to gemfile, migrat eth edatabase

---
+++ spec/requests/api/comment/comment_spec.rb
@@ -0,0 +1,23 @@
+RSpec.describe "Comments", type: :request do
+  let!(:user) { create(:user)}
+  let!(:user_credentials) { user.create_new_auth_token }
+  let!(:user_headers) { { HTTP_ACCEPT: 'application/json'}.merge!(user_credentials)}
+
+  let!(:article) { create(:article)}
+
+  describe 'POST /api/comments' do
+    before do 
+      post '/api/comments',
+      params: {
+        comment: {
+          description: "I realy recommend that article to young parents.",
+          article_id: article.id
+        }
+      }, headers: user_headers
+    end
+
+    it 'is expected to return 200 response status'do
+      expect(response).to have_http_status 200
+    end
+  end
+end 
